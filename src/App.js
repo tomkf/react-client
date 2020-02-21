84a13ebb12eb998231cb0fdfcd4d8aa0940e9faf
tomkf <thomaskeithferris@gmail.com>
2020-02-21 11:10:57 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,59 +1,40 @@
 import React from 'react';
-const BASE_URL        = 'https://localhost:5001/api/';
+const BASE_URL        = 'https://localhost:44362/api/';
+
+const AUTH_TOKEN = "auth_token";
 
 class App extends React.Component {
     
     constructor() {
         super();
         this.state  = {
-          todos : [],
-          singleTodo:{},
-          lastSearched:{}
+          loginMessage : "",
+          token:"",
+          todos:[]
         };
-        this.getAll = this.getAll.bind(this);
-        this.submitToDo = this.submitToDo.bind(this);
-        this.getTodo    = this.getTodo.bind(this);
+        this.login         = this.login.bind(this);
+        this.logout        = this.logout.bind(this);
+        this.getSecureData = this.getSecureData.bind(this);
     }
 
     // Called when constructor is finished building component.
-    componentDidMount() {
-      this.getAll();
+    componentDidMount() {  
+        if(sessionStorage.getItem(AUTH_TOKEN)!=null) {
+          this.setState({ 
+            token:sessionStorage.getItem(AUTH_TOKEN)});
+        }
     }
 
-    getAll() {    
-         // Read from cache by key.
-         let cachedItem = sessionStorage.getItem("MY_KEY");
+    // Executes when button pressed.
+    login(e) {
+      const email      = this.email.value;
+      const password   = this.password.value;
+      const rememberMe = false;
 
-         // As long as something was in the cache…
-         if(cachedItem != null) {
-         // Convert string back to JavaScript object.
-         let cachedObj  = JSON.parse(cachedItem);
-   
-       // Store object on the state.
-       this.setState({lastSearched:cachedObj});
-       }
-
-        const URL        = BASE_URL + 'todo';
-        // This code gets data from the remote server.
-        fetch(URL).then(response => response.json())
-
-        // Data Retrieved.
-        .then((data) => {
-          let serverResponce = JSON.stringify(data)
-          console.log(serverResponce)  
-          this.setState({todos:data});
-        })
-
-        // Data Not Retrieved.
-        .catch((error) => {
-            alert(error);
-        });         
-    }
-
-    submitToDo(e) {
-      const description   = this.ToDo.value;
-      const URL           = BASE_URL + 'todo';
-      this.ToDo.value = ""; // Clear input.     
+      const URL           = BASE_URL + 'login';
+      this.email.value    = ""; // Clear input.  
+      this.password.value = ""; // Clear input. 
+        
       fetch(URL, {
           method: 'POST',
           headers: {
@@ -61,130 +42,103 @@ class App extends React.Component {
               'Content-Type': 'application/json',
           },
           body: JSON.stringify({
-              IsComplete:  false, // Set default to false.
-              Description: description,
+              Email:      email, 
+              Password:   password,
+              RememberMe: rememberMe // Set default to false.
           })
       })
       // Response received.
       .then(response => response.json())
           // Data retrieved.
           .then(json => {
-              alert(JSON.stringify(json));
-              this.getAll();
+              // Store token with session data.
+              if(json["status"]=="OK") {
+                sessionStorage.setItem(AUTH_TOKEN, json["token"]);
+                this.token   = json["token"];
+                console.log(this.token);
+                this.setState({loginMessage:"The user has been logged in.",
+                token: json["token"] }); 
+              }
+              else {
+                this.setState({loginMessage:
+                  "An error occured at login. Try again." }); 
+              }
           })
           // Data not retrieved.
           .catch(function (error) {
+              if(sessionStorage[""])
               alert(error);
           }) 
-  }
-  
-  delete(id) {
-    alert(id);
-    const URL = BASE_URL + 'todo/MyDelete?Id=' + id;
+    }
+
+    logout(e) {
+      alert("Inside logout");
+      if(sessionStorage.getItem([AUTH_TOKEN])!=null) {
+          sessionStorage.clear();
+      }
+      this.setState({todos:[], loginMessage : "", token:""});
+    }
+
+    getSecureData(e) {
+      let token   = sessionStorage.getItem('auth_token');
+      
+
+      const URL        = BASE_URL + 'Login/List';
+
+        // This code gets data from the remote server.
+        // fetch(URL).then(response => response.json())
         fetch(URL, {
-            method: 'DELETE',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-            }
-        })        
-        .then(response => response.json())
-            // Data retrieved.
-            .then(json => {
-                alert(JSON.stringify(json));
-                this.getAll();
-            })
-            // Data not retrieved.
-            .catch(function (error) {
-                alert(error);
-            })             
-}
-
-updateToDo(id, checked) {
-  const URL = BASE_URL + 'todo/MyEdit';
-
-  fetch(URL, {
-      method: 'PUT',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-          Id: id,
-          IsComplete:  checked
-      })
-  })
-  // Wait for response.   
-  .then(response => response.json())
-      // Data retrieved.
-      .then(json => {
-          alert(JSON.stringify(json));
-          this.getAll();
-      })
-      // Data not retrieved.
-      .catch(function (error) {
-          alert(error);
-      }) 
-}
-
-//get by single ID
-getTodo(e) {
-  alert(this.ToDoItem.value);
-  let id = this.ToDoItem.value;
-
-  const URL = BASE_URL + 'todo/' + id;
-      fetch(URL, {
           method: 'GET',
           headers: {
               'Accept': 'application/json',
               'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
           }
-      })        
-      .then(response => response.json())
-          // Data retrieved.
-          .then(json => {
-            let strJSON = JSON.stringify(json);
-            // Store string in cache.
-            sessionStorage.setItem("MY_KEY", strJSON);  
-            this.getAll(); // Refresh list and label.
-              alert(JSON.stringify(json));
-              this.setState({singleTodo : json});
-          })
-          // Data not retrieved.
-          .catch(function (error) {
-              alert(error);
-          })             
-}
+      })
+      .then(res => res.json())
+      // Data Retrieved.
+      .then((data) => {
+          alert(JSON.stringify(data))
+          this.setState({todos:data });
+      })
 
+        // Data Not Retrieved.
+        .catch((error) => {
+            alert(error);
+        });  
+
+    }
     render() {
         return (          
             <div>
-              <ul>
-              {this.state.todos.map((item, index)=>(
-                <li key={item.Id}>{index} {item.id} {item.description}    
-                <input type='checkbox' value={item.isComplete}
-                checked={item.isComplete} 
-                onChange={(e) => this.updateToDo(item.id, e.target.checked)} />
-                 <a href="#" onClick={(e) => this.delete(item.id)}>Delete</a>  </li>
-              ))}
-              </ul>         
-              {/* Create new item */}
-              <input type="text" ref={(toDoInput) => this.ToDo = toDoInput} />
-              <button onClick={this.submitToDo}>Submit New Todo Description</button>          
- 
-             {/* Get item */}
-            <br/><br/>
-            <input type="text" ref={(getTodoInput) => this.ToDoItem = getTodoInput} />
-            <button onClick={this.getTodo}>Get Todo By ID</button>  
-            <br/>
-            {this.state.singleTodo.id} {this.state.singleTodo.description} 
-            { this.state.singleTodo.isComplete ?  ' - Completed.': '' }    
-            { this.state.singleTodo.isComplete==false ?  ' - Not completed.':'' } 
+            <table>
+            <thead>
+              <tr>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td> Email: </td>
+                <td> <input type='text' ref={(emailInput)=> this.email = emailInput}/> </td>
+              </tr>
+              <tr>
+                <td>Password: </td>
+                <td> <input type='text' ref={(passwordInput)=> this.password = passwordInput}/></td>
+              </tr>
+              <tr><td><button onClick={this.login}>Login</button></td><td></td></tr>
+              </tbody>
+            </table>
+            {this.state.loginMessage}<br/>{this.state.token}<br/><br/>
 
-            <br/><br/>
-            <b>Last Searched</b><br/>
-            {this.state.lastSearched.id?this.state.lastSearched.id:""} &nbps;&nbsp;
-            {this.state.lastSearched.description?this.state.lastSearched.description:""}
+            <button onClick={this.getSecureData}>Get Secure Data</button>
+            <ul>
+              {this.state.todos.map((item, index)=>(
+                <li key={item}>{index} {item} {item.description}</li>
+              ))}
+            </ul>  
+            <br/>       
+            <button onClick={this.logout}>Logout</button>
             </div>     
         )
     }
